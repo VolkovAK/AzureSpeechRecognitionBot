11de872041f191bf26e07712d6489cd807682c84
Alexander Volkov <a.volkov@s-telematics.ru>
2021-10-21 02:00:22 +0300

    more calm styles, login added

--- app/asr_app.py
+++ app/asr_app.py
@@ -13,6 +13,8 @@ from flask import (
     send_from_directory
 )
 from werkzeug.utils import secure_filename
+from werkzeug.security import generate_password_hash, check_password_hash
+from functools import wraps
 
 from tasks import celery_app 
 from database import (
@@ -31,21 +33,55 @@ flask_app.config['SESSION_TYPE'] = 'filesystem'
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
+def login_check(f):
+    @wraps(f)
+    def check(*args, **kwargs):
+        auth_hash = request.cookies.get("auth_hash")
+        if auth_hash is None:
+            return redirect("/login/")
+        if check_password_hash(auth_hash, flask_app.config["AUTH_PASS"]) is False:
+            return redirect("/login/")
+        else:
+            return f(*args, **kwargs)
+
+    return check
+
+@flask_app.route("/login/", methods=["GET", "POST"])
+def login():
+    if request.method == "GET":
+        auth_hash = request.cookies.get("auth_hash")
+        if auth_hash is not None:
+            if check_password_hash(auth_hash, flask_app.config["AUTH_PASS"]) is False:
+                return render_template("login.html", status="error")
+            else:
+                return redirect("/")
+        return render_template("login.html")
+    if request.method == "POST":
+        pwd = generate_password_hash(request.form["pwd"])
+        resp = make_response(redirect("/"))
+        resp.set_cookie("auth_hash", pwd, max_age=60*60*24*365)
+        return resp
+
+
 @flask_app.route('/download/<path:filename>', methods=['GET', 'POST'])
+@login_check
 def download(filename):
     directory = os.path.join(flask_app.root_path, 'results')
     return send_from_directory(directory, filename)
 
 @flask_app.route('/try_delete/<path:filename>/<int:id_pk>', methods=['GET', 'POST'])
+@login_check
 def try_delete(filename, id_pk):
     return render_template("delete.html", filename=filename, id_pk=id_pk)
 
 @flask_app.route('/delete/<int:id_pk>', methods=['GET', 'POST'])
+@login_check
 def delete(id_pk):
     delete_record(id_pk)
     return redirect("/")
 
 @flask_app.route("/", methods=["GET", "POST"])
+@login_check
 def index() -> Response:
     if request.method == "GET":
         db_data = get_all_records_sort_date()
@@ -86,6 +122,9 @@ def index() -> Response:
 
 def main():
     flask_app.secret_key = 'super secret key'
+    with open("/run/secrets/auth_key") as f:
+        auth_key = f.read().strip()
+        flask_app.config["AUTH_PASS"] = auth_key
     with open("/run/secrets/azure_key") as f:
         flask_app.config["AZURE_SUB"] = f.read().strip()
     create_table_if_not_exists()
